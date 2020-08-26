e3f010d5e8641ebf869a08caae3fc5ebb4f4862a
Nasroon <6010110668@R301-mini15.local>
2020-08-26 19:58:28 +0700

    third commit

--- Downloads/mid/lib/edit_screen.dart
+++ Downloads/mid/lib/edit_screen.dart
@@ -4,7 +4,68 @@ class EditScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Text("A"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[buildAnswerWidget(), buildNumPadWidget()],
+      ),
     );
+  }
+
+  Widget buildAnswerWidget() {
+    return Container(
+        padding: EdgeInsets.all(16),
+        constraints: BoxConstraints.expand(height: 180),
+        color: Color(0xffecf0f1),
+        child: Align(
+            alignment: Alignment.bottomRight,
+            child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+              Text("8",
+                  style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold))
+            ])));
+  }
+
+  Widget buildNumPadWidget() {
+    return Container(
+        color: Color(0xffecf0f1),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Row(children: <Widget>[
+              buildNumberButton("7"),
+              buildNumberButton("8"),
+              buildNumberButton("9"),
+            ]),
+            Row(children: <Widget>[
+              buildNumberButton("4"),
+              buildNumberButton("5"),
+              buildNumberButton("6"),
+            ]),
+            Row(children: <Widget>[
+              buildNumberButton("1"),
+              buildNumberButton("2"),
+              buildNumberButton("3"),
+            ]),
+            Row(children: <Widget>[
+              buildNumberButton("CLR"),
+              buildNumberButton("0"),
+              buildNumberButton("OK"),
+            ]),
+          ],
+        ));
+  }
+
+  Expanded buildNumberButton(String str) {
+    return Expanded(
+        child: Container(
+            margin: EdgeInsets.all(2),
+            color: Colors.white,
+            height: 70,
+            child: Center(
+                child: Text(str,
+                    style: TextStyle(
+                        fontSize: 32, fontWeight: FontWeight.bold)))));
   }
 }
--- Downloads/mid/lib/home_screen.dart
+++ Downloads/mid/lib/home_screen.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:mid/config/routes.dart';
+
+List<Map> student = [
+  {"name": "JJ", "score": "80"},
+  {"name": "Poon", "score": "77"},
+  {"name": "Geng", "score": "85"},
+  {"name": "John", "score": "60"}
+];
+
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-    );
+        appBar: AppBar(
+          title: Text("My List"),
+        ),
+        body: ListView(
+          padding: const EdgeInsets.all(8),
+          children: <Widget>[
+            for (int index = 0; index < student.length; index++)
+              ListTile(
+                leading: Text("${index+1}"),
+                title: Text("${student[index]["name"]}"),
+                trailing: Text(
+                  "${student[index]["score"]}",
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                ),
+              ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).pushNamed(AppRoutes.edit);
+        },
+        child: Icon(Icons.plus_one),
+        backgroundColor: Colors.green,
+      ),);
   }
 }
